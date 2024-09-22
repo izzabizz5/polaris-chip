4dabbc30b0eff5fc7ba7155819a451dce582cf14
izzabizz5 <154451126+izzabizz5@users.noreply.github.com>
2024-09-22 17:22:22 -0400

    I made changes

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,116 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.card = "";
+    this.title = "";
+    this.blurb = "";
+    this.picture = "#";
+    this.link = "#";
+    this.button="";
+    
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        border: 8px outset #a68a64;
+        margin: 64px;
+        padding: 16px;
+        max-width: 400px;
+        width: auto;
+        border-radius: 6px;
+        background-color: #a4ac86;
+        text-decoration-style:none;
       }
+    h1 {
+      font-family: verdana;
+      text-align: center;
+      color: #132a13;
+    }
+    p {
+      padding: 8px;
+      background-color: #a68a64;
+      color: #132a13;
+      font-family: verdana;
+      text-align: center;
+      font-family: verdana;
+    }
+
+    img {
+      height: 250px;
+      max-width: 370px;
+      width: 100%;
+      margin: auto;
+      border: 6px outset #a68a64;
+      border-radius: 6px;
+    }
+
+    .button{
+      text-align: center;
+      padding: 6px;
+      margin: 6px 0px 0px 0px;
+    }
+
+    #btn {
+      background-color: #a68a64;
+      color: #132a13;
+      padding: 8px 64px;
+      border-color: #a4ac86;
+      border-radius: 6px;
+      border-width: 6px;
+      font-family: verdana;
+    }
+
+    #btn:hover {
+      color: #132a13;
+      background-color: #a4ac86;
+      border-color: #a68a64;
+    }
+    @media (min-width: 500px) and (max-width: 800px) {
+      button {
+        display: inline-block;
+      }
+    }
+    @media (max-width: 500px) {
+      div {
+        max-width: 400px;
+        width: auto;
+      }
+      img {
+        height: 75%;
+        width: 75%;
+      }
+      p, #btn {
+        font-size: 16px;
+      }
+    }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+      <div>
+        <h1>${this.title}</h1>
+        <p>${this.blurb}</p>
+        <img src="${this.picture}" alt="${this.title}">
+        <div class="button">
+          <a href="${this.link}">${this.text} 
+            <button id="btn">${this.button}</button>
+          </a> 
+        </div>
+      </div>
+    `;
   }
 
   static get properties() {
     return {
+      card: { type: String },
       title: { type: String },
+      blurb: { type: String },
+      picture: { type: String},
+      link: { type: String},  
+      button: { type: String}    
     };
   }
 }
